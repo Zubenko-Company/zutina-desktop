0c188a1305651e871908bd915e5afe397b5a7ab6
Anton <69795966+a000011@users.noreply.github.com>
2024-06-02 13:28:14 +0400

    feat: add close context event

--- src/components/desktop/window/folder/file/contextMenu/contextMenu.tsx
+++ src/components/desktop/window/folder/file/contextMenu/contextMenu.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
+import { FC, useContext, useEffect, useRef } from "react";
 import "./contextMenu.css";
+import { DesktopContext } from "../../../..";
 
 const options = [
   {
@@ -20,9 +21,31 @@ export type ContextMenuType = { x: number; y: number; isClosed: boolean };
 
 export const ContextMenu: FC<ContextMenuType> = (props: ContextMenuType) => {
   const display = props.isClosed ? "none" : "block";
+  const contextMenu = useRef<HTMLDivElement>(null);
+  const { setContextMenu } = useContext(DesktopContext);
+
+  useEffect(() => {
+    function handleClickOutside(event: Event) {
+      if (
+        contextMenu.current &&
+        !contextMenu.current.contains(event.target as Node)
+      ) {
+        setContextMenu({
+          x: 0,
+          y: 0,
+          isClosed: true,
+        });
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [contextMenu]);
 
   return (
     <div
+      ref={contextMenu}
       className="contextMenu"
       style={{ left: props.x, top: props.y, display }}
     >
